ca2cbdb93c420358363fd1760c69e3ef83c2aae4
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-23 14:34:32 +0200

    1

--- _itoa.c
+++ _itoa.c
@@ -4,26 +4,30 @@
  * @i: the num
  * @strout: the string to save the num
  * @base: either hexa or octa or dec
- * @Return: string with the int
+ * Return: string with the int
  */
-char * _itoa(int i, char *strout, int base)
+char *_itoa(int i, char *strout, int base)
 {
-  char *str = strout;
- long  int digit, sign = 0;
-  if (i < 0) {
-    sign = 1;
-    i *= -1;
-  }
-  while(i) {
-    digit = i % base;
-    *str = (digit > 9) ? ('A' + digit - 10) : '0' + digit;
-    i = i / base;
-    str ++;
-  }
-  if(sign) {
-  *str++ = '-';
-  }
-  *str = '\0';
-  _strrev(strout);
-  return strout;
+	char *str = strout;
+	long  int digit, sign = 0;
+
+	if (i < 0)
+	{
+		sign = 1;
+		i *= -1;
+	}
+	while (i)
+	{
+		digit = i % base;
+		*str = (digit > 9) ? ('A' + digit - 10) : '0' + digit;
+		i = i / base;
+		str++;
+	}
+	if (sign)
+	{
+		*str++ = '-';
+	}
+	*str = '\0';
+	_strrev(strout);
+	return (strout);
 }
