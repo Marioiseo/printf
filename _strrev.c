0111bf68a802997d9d4796f40b61fc3003d0f6d5
26fati <FatimaZohraDabouz@my.uopeople.edu>
2023-04-23 14:38:57 +0200

    1

--- _strrev.c
+++ _strrev.c
@@ -2,23 +2,25 @@
 /**
  * _strrev - my own function to reverse strings
  * @str: the string
- * @Return: the reversed string
+ * Return: the reversed string
  */
-char *_strrev (char *str)
+char *_strrev(char *str)
 {
- long int i;
- long  int len = 0;
-  char c;
-  if (!str)
-    return NULL;
-  while(str[len] != '\0'){
-    len++;
-  }	
-for(i = 0; i < (len/2); i++)
+	long int i;
+	long  int len = 0;
+	char c;
+
+	if (!str)
+		return (NULL);
+	while (str[len] != '\0')
 	{
-	c = str[i];
-	str [i] = str[len - i - 1];
-	str[len - i - 1] = c;
+		len++;
 	}
-  return str;
+	for (i = 0; i < (len / 2); i++)
+	{
+		c = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = c;
+	}
+	return (str);
 }
