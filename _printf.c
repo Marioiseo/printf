cff049d33413168e21200c4307fef2c09d366ac3
Marioiseo <mariohany302@gmail.com>
2023-04-25 11:35:03 -0700

    drogoo

--- _printf.c
+++ _printf.c
@@ -1,38 +1,50 @@
 #include "main.h"
 
-
 /**
- * _printf - my first advanced implementation
- * @format: its the % i guess
- * Return: int num
+ * _printf - prints anything
+ * @format: the format string
+ *
+ * Return: number of bytes printed
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	va_list pt;
-	int count = 0;
-	char *poi;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-	va_start(pt, format);
-	if (!format || (format[0] == '%' && !format[i]))
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
-	       return (-1);
-	while (format && format[i])
+		return (-1);
+	for (p = (char *)format; *p; p++)
 	{
-		poi = (char *)format;
-		if (*poi == '%')
+		init_params(&params, ap);
+		if (*p != '%')
 		{
-			poi++;
-		count += get_spec(poi, pt);
+			sum += _putchar(*p);
+			continue;
 		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-		{
-		_putchar(*poi);
-		count++;
-		}
-		i++;
+			sum += get_print_func(p, ap, &params);
 	}
-	va_end(pt);
-	return (count);
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
+
--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     int len;
     int len2;
-    int ui;
+    unsigned int ui;
     void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
@@ -39,5 +39,6 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n"); return (0);
+    printf("Unknown:[%r]\n");
+    return (0);
 }
